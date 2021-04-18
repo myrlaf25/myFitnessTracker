5ccf204dc24f103ab6adae680e304b5dac69cf2b
Myrla <75264586+myrlaf25@users.noreply.github.com>
2021-04-18 15:36:11 -0700

    change in model

--- models/workout.js
+++ models/workout.js
@@ -41,7 +41,7 @@ const WorkoutSchema = new Schema({
 });
 
 
-const Workout = mongoose.model("Workout", WorkoutSchema);
+const Workout = mongoose.model("workout", WorkoutSchema);
 
 module.exports = Workout;
 
